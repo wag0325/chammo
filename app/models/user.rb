abf84935ecc7a5909ee9853e4b06fd3f721032fa
Lydia Kwag <wag0325@gmail.com>
2014-01-18 17:57:47 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
 
 	has_secure_password
+	before_save { email.downcase! }
 	validates :password, length: { minimum: 6 }
 end
